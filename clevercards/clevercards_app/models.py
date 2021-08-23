fe3ce2a98faa3fd31c641736677888619ead3c45
kutlo2019 <kutlopopo@rocketmail.com>
2021-08-23 13:29:08 +0200

    add question and result models

--- clevercards/clevercards_app/models.py
+++ clevercards/clevercards_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Quiz(models.Model):
+    name = models.CharField(max_length=120)
+    subject = models.CharField(max_length=120)
+    number_of_questions = models.IntegerField()
+    time = models.IntegerField(help_text="duration of the quiz in minutes")
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return f"{self.name}-{self.subject}"
+
+    def get_questions(self):
+        return self.question_set.all()[:self.number_of_questions]
+
+    class Meta:
+        verbose_name_plural = 'Quizes'
